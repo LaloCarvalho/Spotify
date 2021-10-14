816ba80e71f03a6c3b02fde31374d7cceb8c24a2
Eliard0 <eliardosilva81@gmail.com>
2021-10-14 16:00:11 -0300

    capa e musica atualizadas

--- src/views/oucaGratisDetalhes/index.tsx
+++ src/views/oucaGratisDetalhes/index.tsx
@@ -1,10 +1,10 @@
 import Page from "../../components/page";
 import { useParams } from "react-router-dom";
 import { playLists } from "../oucaGratis/dados";
-import { Container, H1, Img, LinksRight } from "./styles";
+import { Container, H1, Img} from "./styles";
 import ReactAudioPlayer from "react-audio-player";
 import { useEffect } from "react";
-
+ 
 type PlaylistParams = {
   id: string;
   descricao: string;
@@ -14,7 +14,8 @@ type PlaylistParams = {
 export default function OucaGratisDetalhes() {
   const { id } = useParams<PlaylistParams>();
   const playlistEncontrada = playLists.find((p) => p.id === parseInt(id));
-  var songs = ['/samba.mpga', '/forro.mpga', '/reggae.m4a']; 
+  var songs = ['/Atomic Music.mp3','/STAY .mp3','/Linkin Park.mp3']; 
+
   useEffect(() => {
     console.log(playlistEncontrada);
   }, [])
@@ -23,14 +24,13 @@ export default function OucaGratisDetalhes() {
     <Page>
       <Container>
       <H1>
-          Melhores Artistas de :{" "}
+          Melhores musicas de :{" "}
           {playlistEncontrada?.musicas.map((musicas) => (
             <span key={musicas.id}>{musicas.estilo}</span>
           ))}
         </H1>
         <ReactAudioPlayer src={songs[parseInt(id, 10)]} autoPlay controls />
         <br />
-        <LinksRight to="/artista">João de Baixo</LinksRight>
         {playlistEncontrada?.musicas.map((musica) => (
           <Img src={musica.capa} />
         ))}
