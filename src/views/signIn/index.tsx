359798775997478ed14254481cc9a29a42efb9ce
Caio Barroso <caiobarroso511@yahoo.com.br>
2021-11-02 20:51:42 -0300

    using json-server to login

--- src/views/signIn/index.tsx
+++ src/views/signIn/index.tsx
@@ -1,29 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 import Page from '../../components/page';
+import { useHistory } from 'react-router';
 import {
-    ButtonFacebook, ButtonGoogle, ButtonApple,Container, TextInput, H1, Linha, ButtonEntrar, H2,
-  } from './styles';
+    ButtonFacebook, ButtonGoogle, ButtonApple, Container, TextInput, H1, Linha, ButtonEntrar, H2,
+} from './styles';
 
 const SignIn: React.FC = () => {
+
+    const JSON_URL = 'http://localhost:4000/usuarios?email='
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    function handleSubmit(e: { preventDefault: () => void; }) {
+        e.preventDefault()
+
+        axios.get(JSON_URL + email)
+            .then(res => {
+                const user = res.data[0]
+
+                if (user.senha !== senha) {
+                    alert('Senha Inválida')
+                } else {
+                    localStorage.setItem('user-logged-in', JSON.stringify(user))
+                }
+        })
+    }
+
 
 
     return (
         <Page>
             <Container>
-            <H2>Para continuar, faça login no Spotify.</H2>
-            <ButtonFacebook>Continua com Facebook</ButtonFacebook>
-            <ButtonGoogle>Continua com Google</ButtonGoogle>
-            <ButtonApple>Continua com o Apple</ButtonApple>
-            <Linha>ou</Linha>
-            <H1>Endereço de e-mail ou nome de usuário</H1>
-            <TextInput id="outlined-basic" label="Email ou Nome de Usuario" variant="outlined" /><br /><br />
-            <H1>Senha</H1>
-            <TextInput id="outlined-basic" label="Informe sua Senha" variant="outlined" /><br /><br />
-            <u>Esqueceu sua Senha ?</u>
-            <ButtonEntrar>Entrar</ButtonEntrar>
-            <u></u>
-            <H2>Não tem uma conta?</H2>
-            <ButtonApple>Inscrever-se no Sportify</ButtonApple>
+                <H2>Para continuar, faça login no Spotify.</H2>
+                <ButtonFacebook>Continua com Facebook</ButtonFacebook>
+                <ButtonGoogle>Continua com Google</ButtonGoogle>
+                <ButtonApple>Continua com o Apple</ButtonApple>
+                <Linha>ou</Linha>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <H1>Endereço de e-mail ou nome de usuário</H1>
+                    <TextInput id="outlined-basic" label="Email ou Nome de Usuario" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)} /><br /><br />
+                    <H1>Senha</H1>
+                    <TextInput id="outlined-basic" label="Informe sua Senha" variant="outlined" value={senha} onChange={(e) => setSenha(e.target.value)} /><br /><br />
+                    <u>Esqueceu sua Senha ?</u>
+                    <ButtonEntrar>Entrar</ButtonEntrar>
+                </form>
+                <u></u>
+                <H2>Não tem uma conta?</H2>
+                <ButtonApple>Inscrever-se no Sportify</ButtonApple>
             </Container>
         </Page>
     )
